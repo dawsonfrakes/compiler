962b73c4b90dd02594be06ce86394c81d911c38a
Dawson Frakes <dawsonmfrakes@gmail.com>
2024-10-25 16:09:16 -0400

    a lisp?

--- compiler.py
+++ compiler.py
@@ -1,332 +1,192 @@
+class List(list):
+    def __repr__(self) -> str: return '(' + " ".join([repr(member) for member in self]) + ')'
+class Symbol(str):
+    def __repr__(self) -> str: return self
+class Number(float): pass
+class EnumLiteral(str):
+    def __repr__(self) -> str: return self
+Atom = Symbol | EnumLiteral | Number
+Exp = List | Atom
+
+def read(s: list) -> Exp | None:
+    stack = []
+    level = 0
+    while True:
+        while len(s) != 0 and s[-1].isspace(): s.pop()
+        if len(s) == 0: break
+        c = s.pop()
+        if c == '(':
+            stack.append(List())
+            level += 1
+            continue
+        if c == ')':
+            level -= 1
+            x = stack.pop()
+            (stack[-1] if len(stack) != 0 else stack).append(x)
+        elif c == '.':
+            while len(s) > 0 and (s[-1].isalnum() or s[-1] in "+-*/_="): c += s.pop()
+            (stack[-1] if len(stack) != 0 else stack).append(EnumLiteral(c))
+        elif c.isdigit():
+            while len(s) > 0 and (s[-1].isdigit() or s[-1] in "."): c += s.pop()
+            (stack[-1] if len(stack) != 0 else stack).append(Number(c))
+        elif c.isalpha() or c in "+-*/_=":
+            while len(s) > 0 and (s[-1].isalnum() or s[-1] in "+-*/_="): c += s.pop()
+            (stack[-1] if len(stack) != 0 else stack).append(Symbol(c))
+        else: raise NotImplementedError(c)
+        if level == 0 and len(stack) == 1: break
+    assert len(stack) <= 1
+    return stack[0] if len(stack) != 0 else None
+
 from dataclasses import dataclass
-from enum import IntEnum
-
-class TokenKind(IntEnum):
-    INVALID = 0
-    EOF = 128
-    ERROR = 129
-    IDENTIFIER = 130
-    NUMBER = 131
-    STRING = 132
-    TYPE = 133
-
-    KEYWORD_STRUCT = 148
-    KEYWORD_FOREIGN = 149
-    KEYWORD_PROC = 150
-    KEYWORD_CALLCONV = 151
-
-    def as_str(t: int) -> str: return TokenKind(t).name if t in TokenKind else f"'{chr(t)}'"
-
-keywords = {
-    "struct": TokenKind.KEYWORD_STRUCT,
-    "foreign": TokenKind.KEYWORD_FOREIGN,
-    "proc": TokenKind.KEYWORD_PROC,
-    "callconv": TokenKind.KEYWORD_CALLCONV,
-}
 
 @dataclass
-class Token:
-    offset: int
-    length: int
-    kind: int
-
-class Type_Kind(IntEnum):
-    INVALID = 0
-    NORETURN = 1
-    INTEGER = 2
+class Enum:
+    names: list[Symbol]
+    values: list[Exp]
 
 @dataclass
-class Type_Info:
-    kind: Type_Kind
+class EnumField:
+    type_: Exp
+    field: Symbol
 
 @dataclass
-class Type_Info_Noreturn(Type_Info):
-    kind = Type_Kind.NORETURN
+class Proto:
+    params: list[Exp]
+    callconv: Exp
+    ret_type: Exp
 
 @dataclass
-class Type_Info_Integer(Type_Info):
-    kind = Type_Kind.INTEGER
+class Integer:
     bits: int
     signed: bool
 
-type_noreturn = Type_Info_Noreturn(Type_Kind.NORETURN)
-type_c_uint = Type_Info_Integer(Type_Kind.INTEGER, 32, False)
-
-class Code_Node_Kind(IntEnum):
-    INVALID = 0
-    DECLARATION = 1
-    FOREIGN = 2
-    STRUCT = 3
-    PROCEDURE_HEADER = 4
-    PROCEDURE_BODY = 5
-    TYPE = 6
-    IDENT = 7
-    CALL = 8
-    NUMBER = 9
-    FIELD = 10
+@dataclass
+class Noreturn:
+    pass
 
 @dataclass
-class Code_Node:
-    kind: Code_Node_Kind
+class CUint:
+    pass
 
-@dataclass
-class Code_Declaration(Code_Node):
-    kind = Code_Node_Kind.DECLARATION
-    identifier: Token
-    type_expr: Code_Node | None
-    value_expr: Code_Node | None
-    constant: bool
+env = {"c-uint": CUint(), "noreturn": Noreturn(), "CPU_Endianness": Enum([Symbol("x86")], [0.0]), "CPU": EnumField(Symbol("CPU_Endianness"), Symbol("x86"))}
+def doeval(x: Exp) -> Exp | bool | None:
+    if not isinstance(x, List):
+        if isinstance(x, Symbol):
+            result = env[x]
+            return result
+        return x
+    op, *args = x
+    if op == Symbol("const"):
+        name, value = args
+        assert isinstance(name, Symbol)
+        assert name not in env
+        value = doeval(value)
+        env[name] = value
+        return None
+    elif op == Symbol("proto"):
+        params, callconv, ret_type = args
+        params = [doeval(param) for param in params]
+        callconv = doeval(callconv)
+        ret_type = doeval(ret_type)
+        return Proto(params, callconv, ret_type)
+    elif op == Symbol("if"):
+        test, conseq, alt = args
+        return doeval(conseq) if doeval(test) else doeval(alt)
+    elif op == Symbol("enum"):
+        names, values = args[::2], args[1::2]
+        assert all([isinstance(name, Symbol) for name in names])
+        values = [doeval(value) for value in values]
+        return Enum(names, values)
+    elif op == Symbol("=="):
+        lhs, rhs = args
+        lhs = doeval(lhs)
+        rhs = doeval(rhs)
+        return lhs == rhs
+    raise NotImplementedError(op)
 
-@dataclass
-class Code_Foreign(Code_Node):
-    kind = Code_Node_Kind.FOREIGN
-    library: str | None
-    value_expr: Code_Node
+def cify_callconv(x: EnumLiteral) -> str:
+    if x == EnumLiteral(".c"):
+        return "__cdecl"
+    if x == EnumLiteral(".stdcall"):
+        return "__stdcall"
 
-@dataclass
-class Code_Struct(Code_Node):
-    kind = Code_Node_Kind.STRUCT
-    members: list[Code_Node]
+def cify_function(type_: Exp, name: str) -> str:
+    type_ = doeval(type_)
+    arg_list = ", ".join([cify(param) for param in type_.params]) if len(type_.params) > 0 else "void"
+    return cify(type_.ret_type) + " " + cify_callconv(type_.callconv) + " " + repr(name) + "(" + arg_list + ")"
 
-@dataclass
-class Code_Procedure_Header(Code_Node):
-    kind = Code_Node_Kind.PROCEDURE_HEADER
-    arg_names: list[Token]
-    arg_types: list[Code_Node]
-    return_type: Code_Node
-    callconv: Code_Node | None
-
-@dataclass
-class Code_Procedure_Body(Code_Node):
-    kind = Code_Node_Kind.PROCEDURE_BODY
-    header: Code_Procedure_Header
-    statements: list[Code_Node]
-
-@dataclass
-class Code_Type(Code_Node):
-    kind = Code_Node_Kind.TYPE
-    info: Type_Info
-
-@dataclass
-class Code_Ident(Code_Node):
-    kind = Code_Node_Kind.IDENT
-    ident: Token
-
-@dataclass
-class Code_Call(Code_Node):
-    kind = Code_Node_Kind.CALL
-    lhs: Code_Node
-    args: list[Code_Node]
-
-@dataclass
-class Code_Number(Code_Node):
-    kind = Code_Node_Kind.NUMBER
-    value: Token
-
-@dataclass
-class Code_Field(Code_Node):
-    kind = Code_Node_Kind.FIELD
-    path: Code_Node
-    field: Token
-
-class Parser:
-    def __init__(self, s: str) -> None:
-        self.s, self.p = s, 0
-
-    def token_at(self, p: int) -> Token:
-        while p < len(self.s) and self.s[p].isspace(): p += 1
-        kind = TokenKind.INVALID
-        l = 1
-        if p >= len(self.s): kind = TokenKind.EOF
-        elif self.s[p].isalpha() or self.s[p] == '_':
-            kind = TokenKind.IDENTIFIER
-            while p + l < len(self.s) and (self.s[p + l].isalnum() or self.s[p + l] == '_'): l += 1
-            if self.s[p:p + l] in ["c_uint", "noreturn"]:
-                kind = TokenKind.TYPE
-            elif self.s[p:p + l] in keywords:
-                kind = keywords[self.s[p:p + l]]
-        elif self.s[p] == '"':
-            kind = TokenKind.STRING
-            while p + l < len(self.s) and (self.s[p + l - 1] == '\\' or self.s[p + l] != '"'): l += 1
-            if p + l >= len(self.s) or self.s[p + l] != '"':
-                kind = TokenKind.ERROR
-            else: l += 1
-        elif self.s[p].isdigit():
-            kind = TokenKind.NUMBER
-            while p + l < len(self.s) and self.s[p + l].isdigit(): l += 1
-        elif self.s[p] in ".:=+-*/<>(){}[]": kind = ord(self.s[p])
-        else:
-            kind = TokenKind.ERROR
-        return Token(p, l, int(kind))
-
-    def print_all_tokens(self) -> None:
-        while True:
-            token = self.token_at(self.p)
-            if token.kind == TokenKind.EOF: break
-            if token.kind == TokenKind.ERROR:
-                raise ValueError(self.s[token.offset:][:token.length])
-            self.p = token.offset + token.length
-            print(TokenKind.as_str(token.kind), self.s[token.offset:][:token.length])
-
-    def eat(self, expect: int) -> Token:
-        token = self.token_at(self.p)
-        if token.kind != expect: raise ValueError(f"expected {TokenKind.as_str(expect)}, got {token}")
-        self.p = token.offset + token.length
-        return token
-
-    def peek(self, n: int = 1) -> Token:
-        token = None
-        p = self.p
-        for _ in range(n):
-            token = self.token_at(p)
-            p = token.offset + token.length
-        return token
-
-    def parse_expression(self) -> Code_Node:
-        result = None
-        token = self.peek()
-        if token.kind == TokenKind.KEYWORD_FOREIGN:
-            self.eat(TokenKind.KEYWORD_FOREIGN)
-            library = None
-            if self.peek().kind == TokenKind.STRING:
-                library = self.eat(TokenKind.STRING)
-            type_expr = self.parse_expression()
-            result = Code_Foreign(Code_Node_Kind.FOREIGN, library, type_expr)
-        elif token.kind == TokenKind.KEYWORD_PROC:
-            self.eat(TokenKind.KEYWORD_PROC)
-            self.eat(ord('('))
-            arg_names = []
-            arg_types = []
-            while self.peek().kind != ord(')'):
-                if self.peek(2).kind == ord(':'):
-                    arg_names.append(self.eat(TokenKind.IDENTIFIER))
-                    self.eat(ord(':'))
-                arg_types.append(self.parse_expression())
-                if self.peek().kind == ord(','): self.eat(ord(','))
-                else: break
-            self.eat(ord(')'))
-            assert len(arg_names) == len(arg_types) or len(arg_names) == 0
-            callconv = None
-            if self.peek().kind == TokenKind.KEYWORD_CALLCONV:
-                self.eat(TokenKind.KEYWORD_CALLCONV)
-                self.eat(ord('('))
-                callconv = self.parse_expression()
-                self.eat(ord(')'))
-            return_type = self.parse_expression()
-            header = Code_Procedure_Header(Code_Node_Kind.PROCEDURE_HEADER, arg_names, arg_types, return_type, callconv)
-            if self.peek().kind == ord('{'):
-                self.eat(ord('{'))
-                statements = []
-                while self.peek().kind != ord('}'):
-                    statements.append(self.parse_expression())
-                self.eat(ord('}'))
-                result = Code_Procedure_Body(Code_Node_Kind.PROCEDURE_BODY, header, statements)
-            else: result = header
-        elif token.kind == TokenKind.KEYWORD_STRUCT:
-            self.eat(TokenKind.KEYWORD_STRUCT)
-            self.eat(ord('{'))
-            members = []
-            while self.peek().kind != ord('}'):
-                members.append(self.parse_declaration())
-            self.eat(ord('}'))
-            result = Code_Struct(Code_Node_Kind.STRUCT, members)
-        elif token.kind == TokenKind.TYPE:
-            x = self.s[token.offset:][:token.length]
-            if x == "noreturn":
-                self.eat(TokenKind.TYPE)
-                result = Code_Type(Code_Node_Kind.TYPE, type_noreturn)
-            elif x == "c_uint":
-                self.eat(TokenKind.TYPE)
-                result = Code_Type(Code_Node_Kind.TYPE, type_c_uint)
-            else:
-                print(self.s[token.offset:][:token.length])
-        elif token.kind == TokenKind.IDENTIFIER:
-            ident = self.eat(TokenKind.IDENTIFIER)
-            result = Code_Ident(Code_Node_Kind.IDENT, ident)
-        elif token.kind == TokenKind.NUMBER:
-            number = self.eat(TokenKind.NUMBER)
-            result = Code_Number(Code_Node_Kind.NUMBER, number)
-
-        if result is None: raise NotImplementedError(TokenKind.as_str(token.kind))
-
-        if self.peek().kind == ord('.'):
-            self.eat(ord('.'))
-            field = self.eat(TokenKind.IDENTIFIER)
-            result = Code_Field(Code_Node_Kind.FIELD, result, field)
-
-        if self.peek().kind == ord('('):
-            self.eat(ord('('))
-            args = []
-            while self.peek().kind != ord(')'):
-                args.append(self.parse_expression())
-            self.eat(ord(')'))
-            result = Code_Call(Code_Node_Kind.CALL, result, args)
-
-        if result is not None: return result
-        raise NotImplementedError(TokenKind.as_str(token.kind))
-
-    def parse_declaration(self) -> Code_Declaration:
-        identifier = self.eat(TokenKind.IDENTIFIER)
-        self.eat(ord(':'))
-        type_expr = None
-        if self.peek().kind not in [ord(':'), ord('=')]:
-            type_expr = self.parse_expression()
-        constant = False
-        value_expr = None
-        if self.peek().kind in [ord(':'), ord('=')]:
-            constant = self.eat(self.peek().kind).kind == ord(':')
-            value_expr = self.parse_expression()
-        assert type_expr is not None or value_expr is not None
-        return Code_Declaration(Code_Node_Kind.DECLARATION, identifier, type_expr, value_expr, constant)
-
-    def parse_module(self) -> list[Code_Node]:
-        result = []
-        while parser.peek().kind != TokenKind.EOF:
-            decl = parser.parse_declaration()
-            print(decl)
-            result.append(decl)
-        return result
-
-class CVisitor:
-    def __init__(self, parser: Parser) -> None:
-        self.parser = parser
-
-    def visit(self, node: Code_Node):
-        if node.kind == Code_Node_Kind.IDENT:
-            return self.parser.s[node.token.offset:][:node.token.length]
-        if node.kind == Code_Node_Kind.DECLARATION:
-            result = ""
-            name = self.parser.s[node.identifier.offset:][:node.identifier.length]
-            value_expr = node.value_expr
-            if value_expr.kind == Code_Node_Kind.STRUCT:
-                result += f"struct {name} {{\n"
-                result += "};\n\n"
-                return result
-            if value_expr.kind == Code_Node_Kind.PROCEDURE_BODY:
-                result += f"void {name}() {{"
-                result += "}\n\n"
-                return result
-            raise NotImplementedError(value_expr.kind.name)
-        raise NotImplementedError(node.kind.name)
-
-    def visit_module(self, module: list[Code_Node]) -> str:
-        result = ""
-        for node in module:
-            result += self.visit(node)
-        return result
+cify_env = {}
+def cify(x: Exp) -> str:
+    if not isinstance(x, List):
+        if isinstance(x, Noreturn): return "Noreturn"
+        if isinstance(x, CUint): return "unsigned int"
+        if x in cify_env: return cify_env[x]
+        if isinstance(x, Number): return x
+        raise NotImplementedError(x)
+    op, *args = x
+    if op == Symbol("const"):
+        name, value = args
+        assert isinstance(name, Symbol)
+        # value = doeval(value)
+        doeval(x)
+        return None
+        # return f"// #define {name} {value}"
+    elif op == Symbol("extern"):
+        name, type_ = args
+        assert isinstance(name, Symbol)
+        cify_env[name] = repr(name)
+        return cify_function(type_, name) + ";\n"
+    elif op == Symbol("enum"):
+        names, values = args[::2], args[1::2]
+        return f"enum {{ {names} {values} }};"
+    elif op == Symbol("proto"):
+        name, type_ = args
+        assert isinstance(name, Symbol)
+        return cify_function(type_, name)
+    elif op == Symbol("proc"):
+        name, params, callconv, ret_type, *body = args
+        assert isinstance(name, Symbol)
+        fn = cify_function(Proto([doeval(param) for param in params], doeval(callconv), doeval(ret_type)), name)
+        body = [cify(statement) for statement in body]
+        return f"{fn} {{{"\n\t" + ";\n\t".join(body) + ";\n"}}}"
+    elif op == Symbol("if"):
+        test, conseq, alt = args
+        test = cify(test)
+        conseq = cify(conseq)
+        alt = cify(alt)
+        return f"{test} ? {conseq} : {alt}"
+    elif op == Symbol("=="):
+        lhs, rhs = args
+        lhs = cify(lhs)
+        rhs = cify(rhs)
+        return f"{lhs} == {rhs}"
+    else:
+        proc, *pargs = x
+        proc = cify(proc)
+        pargs = [cify(arg) for arg in pargs]
+        return f"{proc}({", ".join([repr(arg) for arg in pargs])})"
+    raise NotImplementedError(op)
 
 src = """
-kernel32 :: struct {
-    ExitProcess :: foreign "kernel32" proc (c_uint) callconv(WINAPI) noreturn
-}
-
-RawEntryPoint :: proc () callconv(WINAPI) noreturn {
-    kernel32.ExitProcess(0)
-}
+(const CallingConvention (enum
+    default 0
+    c 1
+    stdcall 2))
+(const WINAPI (if (== CPU .x86) (field CallingConvention .stdcall) .c))
+(extern ExitProcess (proto (c-uint) WINAPI noreturn))
+(proc RawEntryPoint () WINAPI noreturn
+ (ExitProcess 0))
 """
 
-parser = Parser(src)
-module = parser.parse_module()
-for node in module: print(f"{node}\n")
-visitor = CVisitor(parser)
-print(visitor.visit_module(module))
+cify_preamble = """
+#define Noreturn void
+"""
+print(cify_preamble)
+
+src_rev = list(reversed(src))
+while True:
+    exp = read(src_rev)
+    if exp is None: break
+    # print(exp)
+    result = cify(exp)
+    if result is not None: print(result)
